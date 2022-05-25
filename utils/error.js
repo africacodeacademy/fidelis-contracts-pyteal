70bf2dff0906c918383d47c627e65d62ec68fd5f
Elias <12845983+eliasbayona@users.noreply.github.com>
2022-05-25 10:38:33 +0200

    Update error.js

--- utils/error.js
+++ utils/error.js
@@ -13,7 +13,7 @@ exports.errorParser = (errorObject) => {
     if(['ECONNREFUSED'].includes(errorObject.code)){
         return {"ERROR":`Microservice unreachable at ${errorObject.address}:${errorObject.port}`}
     }else if([400].includes(errorObject.status)){
-        return {"ERROR":errorObject.response.error.text}
+        return {"ERROR":errorObject.response.body.message}
     }else{
         return errorObject
     }
