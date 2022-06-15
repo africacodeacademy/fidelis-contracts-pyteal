b171271ccd8e8bbae57758b72beeb52e2b104df6
Elias <12845983+eliasbayona@users.noreply.github.com>
2022-06-15 21:45:44 +0200

    testnet  init func updates

--- app.js
+++ app.js
@@ -4,67 +4,73 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const lusca = require("lusca");
 const logger = require("morgan");
 
 const app = express();
 
 dotenv.config({
-    path: ".env"
+  path: ".env",
 });
 
-app.use(cors({
-    origin:"*"
-}))
-
 app.use(
-    express.json({
-        limit: "10mb",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
-app.use(bodyParser.json({
-    limit: '10mb'
-}));
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
 
 app.use(
-    bodyParser.urlencoded({
-        extended: false,
-        limit: '10mb'
-    })
+  bodyParser.json({
+    limit: "10mb",
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    limit: "10mb",
+  })
 );
 
 app.use(logger("dev"));
 app.use(xss());
 
 //sanitize requests against special chars, some precaution against NoSQL Injection Attacks
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 app.set("host", "0.0.0.0" || "127.0.0.1");
 app.set("port", process.env.PORT || 8081);
 
 app.use(
-    session({
-        resave: false,
-        saveUninitialized: true,
-        secret:process.env.SESSION_SECRET,
-        cookie: {
-            // maxAge: 1209600000,
-            secure:true
-        }, // two weeks in milliseconds
-
-    })
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      // maxAge: 1209600000,
+      secure: true,
+    }, // two weeks in milliseconds
+  })
 );
 
 app.use(compression());
 
-app.use(function(req,  res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(cookieParser(process.env.SESSION_SECRET));
@@ -72,24 +78,50 @@ app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.disable("x-powered-by");
 
-const MongoDBConnect = require("./utils/db.js")
+const MongoDBConnect = require("./utils/db.js");
 
+const walletController = require("./controllers/walletController");
+const assetController = require("./controllers/assetController");
 
-const walletController = require("./controllers/walletController")
-const assetController =  require("./controllers/assetController")
+app.post(`/mint/trusttokens`, assetController.mintTrustTokensAsset);
+app.post(`/mint/backertokens`, assetController.mintBackerTokensAsset);
+app.post(`/wallet`, walletController.registerUserWallet);
+app.get(`/wallet`, walletController.getWalletDetails);
+app.get(`/wallet/transactions`, walletController.fetchAccountTransactions);
+app.post(`/wallet/send`, walletController.transaferPoints);
+app.get(`/wallets`, walletController.getWallets);
 
+app.get(`/init`, async (req, res, next) => {
+  const algosdk = require("algosdk");
 
-app.post(`/mint/trusttokens`, assetController.mintTrustTokensAsset)
-app.post(`/mint/backertokens`, assetController.mintBackerTokensAsset)
-app.post(`/wallet`, walletController.registerUserWallet)
-app.get(`/wallet`, walletController.getWalletDetails)
-app.get(`/wallet/transactions`, walletController.fetchAccountTransactions)
-app.post(`/wallet/send`, walletController.transaferPoints)
-app.get(`/wallets`, walletController.getWallets)
+  // let algodclient = new algosdk.Algodv2(
+  //   process.env.KDM_TOKEN,
+  //   process.env.ALGODSERVER,
+  //   ALGODPORT.port
+  // );
 
+  console.log(
+    "========================TOKEN_RESERVE_ADDRESS====================="
+  );
+  let account = algosdk.generateAccount();
+  console.log("Account Address: ", account.addr);
 
+  let mn = algosdk.secretKeyToMnemonic(account.sk);
+  console.log("Account Mnemonic: ", mn);
 
+  var sk = algosdk.mnemonicToSecretKey(mn);
+  console.log("Account sk: ", sk);
 
+  console.log("========================ADMIN_ADDRESS=====================");
+  let account2 = algosdk.generateAccount();
+  console.log("Account Address: ", account2.addr);
+
+  let mn2 = algosdk.secretKeyToMnemonic(account2.sk);
+  console.log("Account Mnemonic: ", mn2);
+
+  var sk2 = algosdk.mnemonicToSecretKey(mn2);
+  console.log("Account sk: ", sk2);
+});
 
 // app.get(`/test2`,  async (req, res, next) =>{
 //     const algosdk = require('algosdk');
@@ -109,7 +141,7 @@ app.get(`/wallets`, walletController.getWallets)
 //     // let mnemonic = (await algosdk.secretKeyToMnemonic(accountKey));
 //     // let sk = new Uint8Array(
 //     //     accountKey
-        
+
 //     //   );
 //     // console.log(sk.toString())
 //     // return res.send(sk.toString())
@@ -135,7 +167,7 @@ app.get(`/wallets`, walletController.getWallets)
 //     let mnemonic = (await algosdk.secretKeyToMnemonic(accountKey));
 //     let sk = new Uint8Array(
 //         accountKey
-        
+
 //       );
 //     console.log(sk.toString())
 //     return res.send({mnemonic:mnemonic, sk:sk.toString()})
@@ -143,11 +175,11 @@ app.get(`/wallets`, walletController.getWallets)
 // })
 
 app.listen(app.get("port"), () => {
-    console.log(
-        "App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    if (process.env.NODE_ENV === "development")
-        console.log("  Press CTRL-C to stop\n");
+  console.log(
+    "App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  if (process.env.NODE_ENV === "development")
+    console.log("  Press CTRL-C to stop\n");
 });
